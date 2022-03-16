794213e443598ba506319218ce8376bf6abe02d7
Gabriel Argento <94012846+JulieClaro@users.noreply.github.com>
2022-03-16 13:50:59 -0300

    app01-02

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAsqmTQcdI39odtwsfrJXUUnmZo2EZkwdk",
+    authDomain: "julie-c2c3a.firebaseapp.com",
+    projectId: "julie-c2c3a",
+    storageBucket: "julie-c2c3a.appspot.com",
+    messagingSenderId: "963571583005",
+    appId: "1:963571583005:web:68ea14f3d56ec293159ef4"
+  }
+};
