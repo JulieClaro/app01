d466a211bc61c9740b5e65a4bf1c7f38e0943365
Gabriel Argento <94012846+JulieClaro@users.noreply.github.com>
2022-03-16 15:38:53 -0300

    app01-03

--- src/app/page/faq/faq.page.ts
+++ src/app/page/faq/faq.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { initializeApp } from 'firebase/app';
+import { collection, query, where, onSnapshot, getFirestore } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.page.html',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqPage implements OnInit {
 
+  app = initializeApp(environment.firebase);
+  db = getFirestore();
+  faq: any;
+
   constructor() { }
 
   ngOnInit() {
+
+    const q = query(collection(this.db, 'faq'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      this.faq = [];
+      querySnapshot.forEach((doc) => {
+
+        this.faq.push(doc.data());
+        this.faq.push(doc.id);
+
+      });
+
+      console.log(this.faq);
+    });
+
   }
 
 }
