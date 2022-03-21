997df6fe779c2752decdebcc3c330dff3ef6d434
Julie Claro de Oliveira <julieclaro1@gmail.com>
2022-03-21 13:19:37 -0300

    kkkk

---
+++ src/app/page/view/view.page.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+// Importando dependências
+import { ActivatedRoute } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { doc, getFirestore, onSnapshot, updateDoc } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.page.html',
+  styleUrls: ['./view.page.scss'],
+})
+export class ViewPage implements OnInit {
+
+  // Armazena o Id do artigo vindo da rota
+  public id: string;
+
+  // Conexão com o Firebase
+  app = initializeApp(environment.firebase);
+
+  // Conexão com o banco de dados
+  db = getFirestore();
+
+  // Armazena o artigo completo
+  art: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    onSnapshot(doc(this.db, 'manual', this.id), async (myArt) => {
+
+      // Armazena o artigo em 'art'
+      this.art = myArt.data();
+
+      // console.log(this.art.views);
+
+      // Incrementa 'views' do artigo
+      await updateDoc(doc(this.db, 'manual', this.id), {
+        views: parseInt(this.art.views, 10) + 1
+      });
+
+    });
+
+  }
+
+}
+
+
+
+
