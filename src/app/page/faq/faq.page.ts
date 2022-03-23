7f78ee3b7d2096880700180dcb01248f82c42c5f
Gabriel Argento <94012846+JulieClaro@users.noreply.github.com>
2022-03-23 13:23:03 -0300

    kkk

--- src/app/page/faq/faq.page.ts
+++ src/app/page/faq/faq.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importa todas as dependências necessárias
 import { initializeApp } from 'firebase/app';
 import { collection, query, where, onSnapshot, getFirestore } from 'firebase/firestore';
 import { environment } from 'src/environments/environment';
@@ -11,25 +12,44 @@ import { environment } from 'src/environments/environment';
 })
 export class FaqPage implements OnInit {
 
+  ///// Variáveis da página /////
+
+  // Conexão com o Firebase
   app = initializeApp(environment.firebase);
+
+  // Conexão com o banco de dados
   db = getFirestore();
+
+  // Vai armazenar cada FAQ obtido
   faq: any;
+
+  // Vai armazenar todos os FAQ obtidos para a view
+  faqs: Array<any> = [];
 
   constructor() { }
 
   ngOnInit() {
 
-    const q = query(collection(this.db, 'faq'));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      this.faq = [];
-      querySnapshot.forEach((doc) => {
+    // Conectar ao banco de dados e obtém todos os documentos da coleção 'faq'
+    onSnapshot(query(collection(this.db, 'faq')), (myFaqs) => {
 
-        this.faq.push(doc.data());
-        this.faq.push(doc.id);
+      // Limpa a lista de FAQ para carregar novamente.
+      this.faq = [];
+
+      // Loop que itera cada faq obtida
+      myFaqs.forEach((doc) => {
+
+        // Armazena dados na variável 'faq'
+        this.faq = doc.data();
+
+        // Armazena o ID do documento em 'faq'
+        this.faq.id = doc.id;
+
+        // Adiciona conteúdo de 'faq' em 'faqs' para ser usado na view
+        this.faqs.push(this.faq);
 
       });
 
-      console.log(this.faq);
     });
 
   }
